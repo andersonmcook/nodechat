67b138fe5f4ba2847cbf9ba42d9ce7b80def3bfa
andersonmcook <andersonmcook@gmail.com>
2016-03-03 09:58:16 -0600

    first route

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('hey')
+})
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`)
+})
+
+
+
+
+
+
