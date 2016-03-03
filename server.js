5ff36ef04022caab614c37bf2b88976ca4ccb93f
andersonmcook <andersonmcook@gmail.com>
2016-03-03 10:42:57 -0600

    public folder

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 'use strict'
 
-const app = require('express')()
+const express = require('express')
+const app = express()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const ws = require('socket.io')(server)
 
 const PORT = process.env.PORT || 3000
 
+app.set('view engine', 'jade')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('hey')
+  res.render('index')
 })
 
 server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
 })
 
-
-
-
-
-
+ws.on('connection', socket => {
+  console.log('connection', socket)
+})
